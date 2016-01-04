206d1ba8bedd22d0d79b983fa95b9ba2d7567e95
logu <logu.kirou@gmail.com>
2016-01-04 09:36:55 +0100

    First commit

---
+++ gulp/config/compile/fonts.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+
+gulp.task('fonts', function () {
+    return gulp.src(require('main-bower-files')()
+            .concat('app/fonts/**/*')
+            
+        )
+        .pipe($.filter('**/*.{eot,svg,ttf,woff,woff2}'))
+        .pipe($.flatten())
+        .pipe(gulp.dest('.tmp/fonts'));
+});
