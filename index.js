e018d1efede35cc526190bd25790f2d6538f8f67
logu <logu.kirou@gmail.com>
2016-01-05 22:24:37 +0100

    added simple express http server

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express')
+  , app = express()  
+  , server = require('http').createServer(app)
+  , path = require('path')
+
+// all environments
+app.set('port', process.env.TEST_PORT || 8080);
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Routes
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/public/index.html');
+});
+
+server.listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
